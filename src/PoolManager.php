22789752b74329aad5a067881facba3fd903a23d
戒尺 <itxiao6@qq.com>
2020-10-09 17:34:13 +0800

    归还连接 操作封装

--- src/PoolManager.php
+++ src/PoolManager.php
@@ -2,7 +2,9 @@
 
 
 namespace itxiao6\SwooleDatabase;
+
 use Swoole\Database\PDOPool;
+
 /**
  * 连接池管理(全局)
  * Class PoolManager
@@ -18,9 +20,9 @@ class PoolManager
      * @param string $name
      * @throws \Exception
      */
-    public static function addPool($size = 64,$name='default')
+    public static function addPool($size = 64, $name = 'default')
     {
-        if(isset(self::$pool[$name]) && self::$pool[$name] instanceof PDOPool){
+        if (isset(self::$pool[$name]) && self::$pool[$name] instanceof PDOPool) {
             throw new \Exception('Pool Exist');
         }
         $config = PDOConfig::getConfig($name);
@@ -33,11 +35,23 @@ class PoolManager
      * @return PDOPool
      * @throws \Exception
      */
-    public static function getPool($name='default')
+    public static function getPool($name = 'default')
     {
-        if(!(isset(self::$pool[$name]) && self::$pool[$name] instanceof PDOPool)){
+        if (!(isset(self::$pool[$name]) && self::$pool[$name] instanceof PDOPool)) {
             self::addPool();
         }
         return self::$pool[$name];
+    }
+
+    /**
+     * 归还当前协程内的所有连接
+     * @throws \Exception
+     */
+    public static function recoveryConnection()
+    {
+        $lists = \itxiao6\SwooleDatabase\Utils\Context::get(\itxiao6\SwooleDatabase\PoolManager::class . '_connection') === null ? [] : \itxiao6\SwooleDatabase\Utils\Context::get(\itxiao6\SwooleDatabase\PoolManager::class . '_connection');
+        foreach ($lists as $item) {
+            \itxiao6\SwooleDatabase\PoolManager::getPool($item['name'])->put($item['pdo']);
+        }
     }
 }
